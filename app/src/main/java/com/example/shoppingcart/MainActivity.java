ba698a41217551aa562d8accd1b9fd84e06a55ff
KngArthr <56852100+KngArthr@users.noreply.github.com>
2019-11-26 13:59:54 -0500

    Added Eclipse Java Code

--- app/src/main/java/com/example/shoppingcart/MainActivity.java
+++ app/src/main/java/com/example/shoppingcart/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.shoppingcart;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,20 +9,48 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    ShoppingCart shoppingCart;
+    String userName;
+    String bankAccount;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        try {
+            shoppingCart = new ShoppingCart();
+
+        } catch (UnsupportedFormatException e) {
+            e.printStackTrace();
+        }
 
         Button buttonProceed1 = (Button) findViewById(R.id.buttonProceed1);
+
+
 
         buttonProceed1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 EditText editTextUserName = (EditText) findViewById(R.id.editTextUserName);
                 EditText editTextBudget = (EditText) findViewById(R.id.editTextBudget);
+
+                userName = String.valueOf(editTextUserName);
+                bankAccount = (String.valueOf(editTextBudget));
+
+                //shoppingCart.setUserName(String.valueOf(editTextUserName));
+                //shoppingCart.setBankAccount(String.valueOf(editTextBudget));
+
+                Intent startIntent = new Intent(getApplicationContext(), Central.class);
+                startIntent.putExtra("com.Example.shoppingcart.userName", userName);
+                startIntent.putExtra("com.Example.shoppingcart.bankAccount", bankAccount);
+
+                startActivity(startIntent);
+
+
+
 
             }
         });
